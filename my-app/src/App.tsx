17df1fb699b404833ee966ded739156e64e96c7c
stevenpham98 <stevenpham861998@gmail.com>
2020-07-27 17:05:34 +1200

    update website

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Pepe the Frog
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.google.com/search?q=pepe+the+frog&rlz=1C5CHFA_enNZ759NZ759&sxsrf=ALeKk00X8j99mflsUf82Ne50nFZWjeZE7Q:1595826179855&source=lnms&tbm=isch&sa=X&ved=2ahUKEwif8crk0-zqAhVdwzgGHR3NBzoQ_AUoAXoECBUQAw&biw=1440&bih=766#imgrc=pgC0fF8exDmIWM"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          FeelsBadMan 
         </a>
       </header>
     </div>
