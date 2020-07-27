730efa23ddfffa41417bf210abc887ebc90f2f3d
stevenpham98 <stevenpham861998@gmail.com>
2020-07-27 17:08:42 +1200

    update website

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -12,11 +12,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://www.google.com/search?q=pepe+the+frog&rlz=1C5CHFA_enNZ759NZ759&sxsrf=ALeKk00X8j99mflsUf82Ne50nFZWjeZE7Q:1595826179855&source=lnms&tbm=isch&sa=X&ved=2ahUKEwif8crk0-zqAhVdwzgGHR3NBzoQ_AUoAXoECBUQAw&biw=1440&bih=766#imgrc=pgC0fF8exDmIWM"
+          href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          FeelsBadMan 
+          FeelsBadMan
         </a>
       </header>
     </div>
